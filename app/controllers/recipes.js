5c11b787b3d1185caa4b5ecb0ea3db0b4603471c
Mikey Badr <mfbadr@gmail.com>
2014-08-12 11:26:13 -0500

    index styling, view tweaks

--- app/controllers/recipes.js
+++ app/controllers/recipes.js
@@ -4,7 +4,7 @@ var Recipe = require('../models/recipe');
 
 exports.index = function(req, res){
   Recipe.all(function(err, recipes){
-    console.log(recipes);
+    //console.log(recipes);
     res.render('recipes/index', {recipes:recipes});
   });
 };
